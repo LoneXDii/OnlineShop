9f63113c04504c9c92735cf428345633a9b6c315
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-11-26 09:25:16 +0300

    Get all categories request implemented

---
+++ ProductsService/ProductsService.API/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductsService.Application.DTO;
+using ProductsService.Application.UseCases.CategoryUseCases.Queries;
+
+namespace ProductsService.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CategoriesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<CategoryDTO>>> GetCategories(CancellationToken cancellationToken)
+    {
+        var categories = await _mediator.Send(new GetAllCategoriesReguest(), cancellationToken);
+
+        return Ok(categories);
+    }
+}
---
+++ ProductsService/ProductsService.Application/UseCases/CategoryUseCases/Queries/GetAllCategoriesRequestHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MediatR;
+using ProductsService.Application.DTO;
+using ProductsService.Domain.Abstractions.Database;
+
+namespace ProductsService.Application.UseCases.CategoryUseCases.Queries;
+
+internal class GetAllCategoriesRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    : IRequestHandler<GetAllCategoriesReguest, List<CategoryDTO>>
+{
+    public async Task<List<CategoryDTO>> Handle(GetAllCategoriesReguest request, CancellationToken cancellationToken)
+    {
+        var categories = await unitOfWork.CategoryQueryRepository.ListAllAsync(cancellationToken);
+
+        var categoriesDto = mapper.Map<List<CategoryDTO>>(categories);
+
+        return categoriesDto;
+    }
+}
