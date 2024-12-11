b740e7780e93222b3f17ad5002329d02ecf5e98c
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-12-11 14:52:01 +0300

    Entities configuration is in different files now

---
+++ ProductsService/ProductsService.Infrastructure/Data/Configuration/DiscountConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductsService.Domain.Entities;
+using System.Reflection.Emit;
+
+namespace ProductsService.Infrastructure.Data.Configuration;
+
+internal class DiscountConfiguration : IEntityTypeConfiguration<Discount>
+{
+    public void Configure(EntityTypeBuilder<Discount> builder)
+    {
+        builder.HasData(
+            new Discount
+            {
+                Id = 1,
+                Percent = 15,
+                ProductId = 3
+            }
+        );
+    }
+}
