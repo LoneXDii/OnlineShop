774884cf205ec95749455124ea376de3acba05d3
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-12-23 09:48:46 +0300

    Extension method for IQueryable for including many navigation propeties in 1 call was added

---
+++ ProductsService/ProductsService.Infrastructure/Extensions/IQueryableExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ProductsService.Infrastructure.Extensions;
+
+internal static class IQueryableExtension
+{
+    public static IQueryable<T> Include<T>(this IQueryable<T> query, Expression<Func<T, object>>[] includedProperties) where T : class
+    {
+        foreach (var property in includedProperties)
+        {
+            query = query.Include(property);
+        }
+
+        return query;
+    }
+}
