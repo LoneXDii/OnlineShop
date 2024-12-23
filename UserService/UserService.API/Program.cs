d0c273511fe74c13da29da2db5a8ad6ed2c38711
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-12-23 15:29:44 +0300

    Commented code from previous prs was uncommented

--- UserService/UserService.API/Program.cs
+++ UserService/UserService.API/Program.cs
@@ -37,13 +37,11 @@ app.MapControllers();
 
 app.UseHangfireDashboard("/dashboard");
 
-//Comment this because it causes conflicts with new db schema from feature/add-kafka 
-//(stripeId field were added to AppUser)
-//using (var scope = app.Services.CreateScope())
-//{
-//    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-//    await dbContext.Database.MigrateAsync();
-//}
+    await dbContext.Database.MigrateAsync();
+}
 
 app.Run();
