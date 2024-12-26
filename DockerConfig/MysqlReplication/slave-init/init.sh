f509ec9f4cd2b0b38ee93fda749ec535e7b3c5d4
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-12-26 13:58:43 +0300

    unnecessary comments were deleted

--- DockerConfig/MysqlReplication/slave-init/init.sh
+++ DockerConfig/MysqlReplication/slave-init/init.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-# Ждем, пока мастер станет доступен
 until mysql -uroot -proot -h products-master -e "SELECT 1"; do
   echo "Waiting for master..."
   sleep 5
 done
 
-# Получаем информацию о мастере
 MASTER_STATUS=$(mysql -uroot -proot -h products-master -e "SHOW MASTER STATUS;" | awk 'NR==2 {print $1, $2}')
 MASTER_LOG_FILE=$(echo $MASTER_STATUS | awk '{print $1}')
 MASTER_LOG_POS=$(echo $MASTER_STATUS | awk '{print $2}')
 
-# Настройка слейва
 mysql -uroot -proot <<EOF
 CHANGE MASTER TO
   MASTER_HOST='products-master',
