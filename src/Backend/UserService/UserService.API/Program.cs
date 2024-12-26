19160a08ad9f2e07d47b44e3174692cc980b4791
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-12-26 17:36:38 +0300

    User service is configured for docker now

--- src/Backend/UserService/UserService.API/Program.cs
+++ src/Backend/UserService/UserService.API/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddApplication(builder.Configuration)
 
 builder.Host.UseLogging();
 
+builder.Configuration.AddUserSecrets<Program>();
+
 var app = builder.Build();
 
 app.UseMiddleware<LoggingMiddleware>();
@@ -27,11 +29,11 @@ app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
