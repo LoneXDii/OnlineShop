91d956508bcbe9fdf4464266ccea07c98717ef61
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2025-01-11 10:07:49 +0300

    Endpoints for roles managing were added to user service

---
+++ src/Backend/UserService/UserService.BLL/UseCases/UserUseCases/RemoveUserFromAdmins/RemoveUserFromAdminsRequest.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace UserService.BLL.UseCases.UserUseCases.RemoveUserFromAdmins;
+
+public sealed record RemoveUserFromAdminsRequest(string UserId) : IRequest { }
---
+++ src/Backend/UserService/UserService.BLL/UseCases/UserUseCases/RemoveUserFromAdmins/RemoveUserFromAdminsRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using UserService.BLL.Exceptions;
+using UserService.DAL.Entities;
+
+namespace UserService.BLL.UseCases.UserUseCases.RemoveUserFromAdmins;
+
+internal class RemoveUserFromAdminsRequestHandler(UserManager<AppUser> userManager, 
+    ILogger<RemoveUserFromAdminsRequestHandler> logger)
+    : IRequestHandler<RemoveUserFromAdminsRequest>
+{
+    public async Task Handle(RemoveUserFromAdminsRequest request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Trying to remove user with id: {request.UserId} from admins");
+
+        var user = await userManager.FindByIdAsync(request.UserId);
+
+        if (user is null)
+        {
+            logger.LogError($"User with id={request.UserId} does not exists");
+
+            throw new NotFoundException("No such user");
+        }
+
+        await userManager.RemoveFromRoleAsync(user, "admin");
+    }
+}
