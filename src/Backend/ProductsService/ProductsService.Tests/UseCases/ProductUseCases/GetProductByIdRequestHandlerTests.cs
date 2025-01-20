41783a311f37fd305db437003572e000be669118
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2025-01-20 17:23:21 +0300

    All use cases from products service were covered by unit tests

---
+++ src/Backend/ProductsService/ProductsService.Tests/UseCases/ProductUseCases/GetProductByIdRequestHandlerTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq.Expressions;
+using AutoFixture;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Moq;
+using ProductsService.Application.DTO;
+using ProductsService.Application.Exceptions;
+using ProductsService.Application.UseCases.ProductUseCases.Queries.GetProductById;
+using ProductsService.Domain.Abstractions.Database;
+using ProductsService.Domain.Entities;
+using ProductsService.Tests.Factories;
+
+namespace ProductsService.Tests.UseCases.ProductUseCases;
+
+public class GetProductByIdRequestHandlerTests
+{
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
+    private readonly Mock<IMapper> _mapperMock = new();
+    private readonly Mock<ILogger<GetProductByIdRequestHandler>> _loggerMock = new();
+    private readonly Fixture _fixture = new();
+    private readonly GetProductByIdRequestHandler _handler;
+
+    public GetProductByIdRequestHandlerTests()
+    {
+        _handler = new GetProductByIdRequestHandler(_unitOfWorkMock.Object, _mapperMock.Object, _loggerMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_WhenProductExists_ShouldReturnProductDto()
+    {
+        //Arrange
+        var request = _fixture.Create<GetProductByIdRequest>();
+        var product = EntityFactory.CreateProduct();
+        var productDto = _fixture.Create<ResponseProductDTO>();
+
+        _unitOfWorkMock.Setup(unitOfWork =>
+            unitOfWork.ProductQueryRepository.GetByIdAsync(request.ProductId, It.IsAny<CancellationToken>(), It.IsAny<Expression<Func<Product, object>>[]>()))
+            .ReturnsAsync(product);
+
+        _mapperMock.Setup(mapper => mapper.Map<ResponseProductDTO>(product)).Returns(productDto);
+
+        //Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(productDto, result);
+    }
+
+    [Fact]
+    public async Task Handle_WhenProductDoesNotExist_ShouldThrowNotFoundException()
+    {
+        //Arrange
+        var request = _fixture.Create<GetProductByIdRequest>();
+
+        _unitOfWorkMock.Setup(unitOfWork =>
+                unitOfWork.ProductQueryRepository.GetByIdAsync(request.ProductId, It.IsAny<CancellationToken>(), It.IsAny<Expression<Func<Product, object>>[]>()))
+            .ReturnsAsync((Product?)null);
+
+        //Act
+        var exception = await Assert.ThrowsAsync<NotFoundException>(() => _handler.Handle(request, CancellationToken.None));
+
+        //Assert
+        Assert.Equal("No such product", exception.Message);
+    }
+}
---
+++ src/Backend/ProductsService/ProductsService.Tests/UseCases/ProductUseCases/TakeProductsRequestHandlerTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq.Expressions;
+using AutoFixture;
+using Microsoft.Extensions.Logging;
+using Moq;
+using ProductsService.Application.Exceptions;
+using ProductsService.Application.UseCases.ProductUseCases.Queries.TakeProducts;
+using ProductsService.Domain.Abstractions.Database;
+using ProductsService.Domain.Abstractions.Specifications;
+using ProductsService.Domain.Entities;
+using ProductsService.Tests.Setups;
+
+namespace ProductsService.Tests.UseCases.ProductUseCases;
+
+public class TakeProductsRequestHandlerTests
+{
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
+    private readonly Mock<ILogger<TakeProductsRequestHandler>> _loggerMock = new();
+    private readonly Fixture _fixture = new();
+    private readonly TakeProductsRequestHandler _handler;
+
+    public TakeProductsRequestHandlerTests()
+    {
+        _unitOfWorkMock.SetupUnitOfWork();
+        
+        _handler = new TakeProductsRequestHandler(_unitOfWorkMock.Object, _loggerMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_WhenProductsAreAvailable_ShouldUpdateQuantities()
+    {
+        //Arrange
+        var request = _fixture.Build<TakeProductsRequest>()
+            .With(req => req.Products, new Dictionary<int, int>{ { 1, 3 }, { 2, 2 } })
+            .Create();
+
+        var products = new List<Product>
+        {
+            new Product { Id = 1, Quantity = 5 },
+            new Product { Id = 2, Quantity = 5 }
+        };
+
+        _unitOfWorkMock.Setup(unitOfWork =>
+            unitOfWork.ProductQueryRepository.ListAsync(
+                It.IsAny<ISpecification<Product>>(), It.IsAny<CancellationToken>(), It.IsAny<Expression<Func<Product, object>>>()))
+            .ReturnsAsync(products);
+
+        //Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Count());
+        Assert.Equal(3, products[0].Quantity);
+        Assert.Equal(2, products[1].Quantity); 
+        _unitOfWorkMock.Verify(unitOfWork => unitOfWork.ProductCommandRepository.UpdateAsync(It.IsAny<Product>(), It.IsAny<CancellationToken>()), Times.Exactly(2));
+        _unitOfWorkMock.Verify(unitOfWork => unitOfWork.SaveAllAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_WhenProductQuantityIsTooLow_ShouldThrowBadRequestException()
+    {
+        //Arrange
+        var request = _fixture.Build<TakeProductsRequest>()
+            .With(req => req.Products, new Dictionary<int, int>{ { 1, 10 } })
+            .Create();
+        
+        var products = new List<Product>
+        {
+            new Product { Id = 1, Quantity = 5 } 
+        };
+
+        _unitOfWorkMock.Setup(unitOfWork =>
+            unitOfWork.ProductQueryRepository.ListAsync(
+                It.IsAny<ISpecification<Product>>(), It.IsAny<CancellationToken>(), It.IsAny<Expression<Func<Product, object>>>()))
+            .ReturnsAsync(products);
+
+        //Act
+        var exception = await Assert.ThrowsAsync<BadRequestException>(() => _handler.Handle(request, CancellationToken.None));
+
+        //Assert
+        Assert.Contains("Quantity of product with id=1 is too low", exception.Message);
+    }
+}
