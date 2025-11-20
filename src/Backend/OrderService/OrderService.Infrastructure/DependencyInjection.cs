a6f11ab74cd6dd31b7d0e7e322ad670e45bce031
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2025-11-20 11:25:49 +0300

    Fix dependencies and add docs

--- src/Backend/OrderService/OrderService.Infrastructure/DependencyInjection.cs
+++ src/Backend/OrderService/OrderService.Infrastructure/DependencyInjection.cs
@@ -17,6 +17,7 @@ using Confluent.Kafka;
 using OrderService.Infrastructure.Proxy;
 using OrderService.Infrastructure.Services.MessageBrocker.Consumers;
 using OrderService.Infrastructure.Services.MessageBrocker;
+using Stripe.Checkout;
 
 
 namespace OrderService.Infrastructure;
@@ -34,6 +35,7 @@ public static class DependencyInjection
             .AddScoped<ProductService>()
             .AddScoped<PriceService>()
             .AddScoped<CouponService>()
+            .AddScoped<SessionService>()
             .AddScoped<IStripeEventUtilityProxy, StripeEventUtilityProxy>();
 
         services.AddSingleton<IOrderRepository, OrderRepository>()
